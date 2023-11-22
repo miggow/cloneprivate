a2a34d78a8141065aea567593972e6c686e4711a
queestraan <queestraan0211@gmail.com>
2023-11-23 01:08:10 +0700

    home page suong suong

--- js.js
+++ js.js
@@ -1,3 +1,29 @@
+jQuery(document).ready(function ($) {
+    $(".slick-blog").slick({
+        dots: false,
+        infinite: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+    });
+});
+
+
+jQuery(document).ready(function ($) {
+    $(".slick-product").slick({
+        dots: false,
+        infinite: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+    });
+});
+
+
 AOS.init({
     once: true,
 });
