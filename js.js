2f59ace10b0daa8c2dd1c102f6e931481ab63944
queestraan <queestraan0211@gmail.com>
2023-11-25 05:26:08 +0700

    cai nay moi dung

--- js.js
+++ js.js
@@ -77,14 +77,4 @@ $(window).scroll(function () {
     }
 });
 
-// init Masonry
-var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer',
-});
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress(function () {
-    $grid.masonry();
-});
 
