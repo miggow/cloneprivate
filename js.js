b93582cfbf6e66d5e102cbdc1d994eab20a9dd0f
queestraan <queestraan0211@gmail.com>
2023-12-09 02:36:15 +0700

    99%

--- js.js
+++ js.js
@@ -36,11 +36,17 @@ jQuery(document).ready(function ($) {
         dots: false,
         infinite: true,
         autoplaySpeed: 3000,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         arrows: false,
-        responsive: [         
+        responsive: [    
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },     
             {
                 breakpoint: 769,
                 settings: {
@@ -66,19 +72,17 @@ jQuery(document).ready(function ($) {
         arrows: true,
         prevArrow: "<div class='icon-prev'> <i class='bi bi-chevron-compact-left'></></div>",
         nextArrow: "<div class='icon-next'> <i class='bi bi-chevron-compact-right'></></div>",
-         
-         
         responsive: [
             {
                 breakpoint: 769,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 4,
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 3,
                 }
             }
         ]
@@ -86,26 +90,12 @@ jQuery(document).ready(function ($) {
 });
 
 
-AOS.init({
-    once: true,
-});
+AOS.init({});
 
 Fancybox.bind("[data-fancybox]", {});
 
 
 
-/* Menu fixed */
-$(window).scroll(function () {
-    var cach_top = $(window).scrollTop();
-    var heaigt_header = $(".big-menu").height();
 
-    if (cach_top >= heaigt_header) {
-        if (!$(".big-menu").hasClass("fix_head animate__animated animate__fadeIn")) {
-            $(".big-menu").addClass("fix_head animate__animated animate__fadeIn");
-        }
-    } else {
-        $(".big-menu").removeClass("fix_head animate__animated animate__fadeIn");
-    }
-});
 
 
