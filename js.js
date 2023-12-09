851adc0158d789908c431cd4af54e1ee628e96aa
queestraan <queestraan0211@gmail.com>
2023-12-09 15:21:23 +0700

    99.6%

--- js.js
+++ js.js
@@ -90,7 +90,18 @@ jQuery(document).ready(function ($) {
 });
 
 
-AOS.init({});
+
+
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer',
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress(function () {
+    $grid.masonry();
+});  
+
 
 Fancybox.bind("[data-fancybox]", {});
 
